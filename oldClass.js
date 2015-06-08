70fe4404d1d7eece3e6f5c1bf3344f6fbc2ddf34
ethics13 <ethics@gmail.com>
2015-06-07 21:42:24 -0400

    OldSchool default

---
+++ oldClass.js
@@ -0,0 +1,12 @@
+// angular.module('OldClass').controller('myController', function (){
+angular.module('OldClass', [])
+.controller('myController', function() {
+  var self = this;
+
+this.jobs= {
+	employee: "employee text",
+	ceo: "CEO text",
+	founder: "Founder text" 
+}
+
+});
